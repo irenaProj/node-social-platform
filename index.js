9406075aa5a10d7734844365457eafd4151a6be0
irena-shemesh <irena@cd.com.au>
2015-07-26 09:55:43 +1000

    Expand functionality - in progress

--- index.js
+++ index.js
@@ -1,7 +1,96 @@
 var express = require('express');
 var fixtures = require('./fixtures.js');
-
 var app = express();
+var bodyParser = require('body-parser');
+var id = 4;
+
+app.get('/api/tweets/:tweetId', function(req, res) {
+  var tweetId = req.params.tweetId;
+  var tweet = null;
+
+  for (var i = 0; i < fixtures.tweets.length; i++ ) {
+    if (fixtures.tweets[i].id === tweetId) {
+      tweet = fixtures.tweets[i];
+    }
+  }
+
+  if (!tweet) {
+    return res.sendStatus(404);
+  } else {
+    return res.send({
+      tweet: tweet
+    });
+  }
+});
+
+app.post('/api/tweets', bodyParser.json(), function(req, res) {
+  console.log('In the post for /api/tweets');
+
+  if (!req.body) {
+    return res.sendStatus(400);
+  }
+
+  var tweet = req.body.tweet;
+
+  tweet.id = id;
+  id++;
+  tweet.created = Math.floor(Date.now() / 1000);
+
+  fixtures.tweets.push(tweet);
+
+  return res.status(200).send({
+      tweet: tweet
+    });
+});
+
+
+app.post('/api/users', bodyParser.json(), function(req, res) {
+  console.log('In the post for /api/users');
+
+  if (!req.body) {
+    return res.sendStatus(400);
+  }
+
+  var user = req.body.user;
+  user.followingIds = [];
+
+  console.log(user);
+
+  // Check user does not exist
+  for (var i = 0; i < fixtures.users.length; i++ ) {
+    if (fixtures.users[i].id === user.id) {
+      return res.sendStatus(409);
+    }
+  }
+
+  fixtures.users.push(user);
+
+  return res.sendStatus(200);
+});
+
+app.delete('/api/tweets/:tweetId', function(req, res) {
+  var tweetId = req.params.tweetId;
+  var index = -1;
+
+  for (var i = 0; i < fixtures.tweets.length; i++ ) {
+    if (fixtures.tweets[i].id === tweetId) {
+      index = i;
+      console.log(fixtures.tweets[i]);
+
+      break;
+    }
+  }
+
+  if (index === -1) {
+    return res.sendStatus(404);
+  } else {
+    fixtures.tweets.splice(index, 1);
+    console.log(fixtures.tweets);
+
+    return res.sendStatus(200);
+  }
+});
+
 
 app.get('/api/users/:userId', function(req, res) {
   var userId = req.params.userId;
@@ -9,7 +98,7 @@ app.get('/api/users/:userId', function(req, res) {
 
   for (var i = 0; i < fixtures.users.length; i++ ) {
     if (fixtures.users[i].id === userId) {
-      user = fixtures.users[i];
+      user = fixtures.users[i];      
     }
   }
 
